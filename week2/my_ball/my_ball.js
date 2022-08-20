f0c037c2d084cd2689c8dae4ea9a2d14373cbd76
Sibylle Peter <sibylle.peter@fhnw.ch>
2022-08-20 20:14:12 +0200

    refactored position and introduced directions

--- week2/my_ball/my_ball.js
+++ week2/my_ball/my_ball.js
@@ -5,7 +5,9 @@
 // Und ich muss mir nur 20 Plätze merken.
 // je nachdem wie ein Kreis gebaut wird, muss ich mir aber 10 Stellen merken.
 
-let my_ball = {x:1, y:1}
+let my_ball = {x:1, y:1};
+let direction = {h:'right', v:'down'};
+const radius = 9;
 
 function start() {
     const canvas  = document.getElementById("canvas");
@@ -19,16 +21,46 @@ function start() {
 
 }
 
+// needed to test
 function same_position(ball, pos){
     return ball.x === pos.x && ball.y === pos.y;
 }
 
-function move(ball){
-    let dx = ball.y/80;
-    ball.x += 7-dx;
-    ball.y +=ball.y >7 ?ball.y/7:1; //Die Geschwindigkeit nimmt in 5er Schritten zu
+function right(ball) {
+    let dx = ball.y / 80;
+    ball.x += 7 - dx;
+}
+function left(ball) {
+    let dx = ball.y / 80;
+    ball.x -= 7 - dx;
 }
 
+function down(y){
+    return y > 10? y+y/10:y+1;
+}
+
+function up(y){
+    return y > 10? y-(y/10):y-1;
+}
+
+const horizontal = ['right', 'left', 'right'];
+const vertical = ['down', 'up', 'down'];
+
+function switchHorizontal(){
+    const dir = horizontal.indexOf(direction.h);
+    direction.h = horizontal[dir+1];
+}
+
+function switchVertical(){
+    const dir = vertical.indexOf(direction.v);
+    direction.v = vertical[dir+1];
+}
+
+function move(ball){
+    // TODO make equal: the vertical functions are better testable
+    direction.h === 'right'?right(ball):left(ball);
+    ball.y = direction.v === 'down'? down(ball.y): up(ball.y);
+}
 
 function display(context){
     // clear canvas
@@ -37,6 +69,6 @@ function display(context){
     // paint ball
     context.fillStyle = "black";
     context.beginPath();
-    context.arc(my_ball.x, my_ball.y, 18, 0, 2 * Math.PI);
+    context.arc(my_ball.x, my_ball.y, radius, 0, 2 * Math.PI);
     context.fill();
 }
